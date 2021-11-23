0ebb7bee437bac4403e018053e95bd2ff708d9cf
itsokthen <cades117@gmail.com>
2021-11-22 20:01:52 -0500

    few changes

--- tagprocentral/src/components/Scoreboard/Recent_Games.js
+++ tagprocentral/src/components/Scoreboard/Recent_Games.js
@@ -6,6 +6,17 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { makeStyles } from "@material-ui/core";
+import { style } from "@mui/system";
+
+const useStyles = makeStyles({
+  card: {
+    borderStyle: "solid",
+  },
+  scoreCarousel: {
+    maxWidth: "100px",
+  },
+});
 
 function createData(name, game1, game2) {
   return { name, game1, game2 };
@@ -14,39 +25,43 @@ function createData(name, game1, game2) {
 const rows = [createData("RNK", 12, 8), createData("HGW", 4, 4)];
 
 export default function DenseTable() {
+  const classes = useStyles();
   return (
-    <TableContainer
-      sx={{
-        [`& .${tableCellClasses.root}`]: {
-          borderBottom: "none",
-        },
-        maxWidth: 160,
-      }}
-      component={Paper}
-    >
-      <Table sx={{ maxWidth: 150 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Final</TableCell>
-            <TableCell align="right"></TableCell>
-            <TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.game1}</TableCell>
-              <TableCell align="right">{row.game2}</TableCell>
+    <>
+      <TableContainer
+        className={classes.card}
+        sx={{
+          [`& .${tableCellClasses.root}`]: {
+            borderBottom: "none",
+          },
+          maxWidth: 160,
+        }}
+        component={Paper}
+      >
+        <Table sx={{ maxWidth: 150 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Final</TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.game1}</TableCell>
+                <TableCell align="right">{row.game2}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
